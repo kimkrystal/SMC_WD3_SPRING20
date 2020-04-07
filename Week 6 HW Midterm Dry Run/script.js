6b3c4b8de92ba83586af79590ce5fe9fbfaddcf2
Krystal <kim_krystal@student.smc.edu>
2020-04-06 22:29:41 -0700

    Week 6 Midterm Dry Run
    Homepage with carousel and cards, about page with featurettes added

---
+++ Week 6 HW Midterm Dry Run/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    //we write everything here
+
+    $('#myCarousel').carousel({
+        interval: 1000
+    });
+
+    $('#carouselControl').click(function () {
+
+        if ($('#carouselControl').hasClass('paused')) {
+            $('#myCarousel').carousel('cycle');
+            $('#carouselControl').text('Pause');
+        } else {
+            $('#myCarousel').carousel('pause');
+            $('#carouselControl').text('Play');
+        }
+
+        $('#carouselControl').toggleClass('paused');
+    });
+
+
+    $('#modalLauncher').click(function () {
+        $('#exampleModal').modal({
+            backdrop: false
+        });
+    });
+});
